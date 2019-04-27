2ca75d3a502b7fc35475a3fbd5df1eef71f8f754
Inna Mon <inna.mon@gmail.com>
2019-04-27 14:21:57 -0500

    added function in javascript that will extract json data and display in html but first item in data array is returning as undefined

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-const finances = document.getElementById('finances');
 const url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=EVV431001NUOLKDZ";
 
 fetch(url)
 .then(function(response) {
+  console.log(response);
   return response.json();
 })
 .then(function(data) {
   console.log(data);
+  appendData(data)
 })
 .catch(function(error) {
   console.log('Error!', error);
 });
 
-// fetch(url, {
-//     mode: 'no-cors'
-//     })
-// // .then(function(response) {
-// //   // Read the response as json.
-// //   return response.json();
-// // })
-// .then(function(data) {
-//   // Do stuff with the JSON
-//   console.log(data);
-// })
-// .catch(function(error) {
-//   console.log('Error!', error);
-// });
+function appendData(data) {
+  const finances = document.getElementById('finances');
+  const div = document.createElement('div');
+  div.innerHTML = data[0];
+  finances.appendChild(div);
+}
