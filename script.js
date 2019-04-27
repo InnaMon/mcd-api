8f7ab019925b15ccb4080f6bfbab674faadd403b
Inna Mon <inna.mon@gmail.com>
2019-04-26 19:42:32 -0500

    added alphantage api in place of google api due to better documentation and maintainability

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const finances = document.getElementById('finances');
-const url = "https://www.google.com/async/finance_wholepage_price_updates?ei=ESbDXJXfB47cswXnlbWwDQ&yv=3&async=mids:%2Fm%2F07zkrcn%7C%2Fm%2F07zltq7%7C%2Fm%2F07zlv08%7C%2Fm%2F07zl90k%7C%2Fm%2F07zlm7d%7C%2Fm%2F07zltf3%7C%2Fm%2F03p3lxb%7C%2Fm%2F07zl5yq%7C%2Fm%2F016yss%7C%2Fm%2F02853rl%7C%2Fm%2F04zvfw%7C%2Fg%2F1q52gbb7v,currencies:,_fmt:jspb";
-
+const url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=EVV431001NUOLKDZ";
 
 fetch(url, {
-    mode: 'no-cors' // 'cors' by default
-  })
+    mode: 'no-cors'
+    })
+// .then(function(response) {
+//   // Read the response as json.
+//   return response.json();
+// })
 .then(function(data) {
-    console.log(data);
+  // Do stuff with the JSON
+  console.log(data);
 })
 .catch(function(error) {
-    console.log('Error!', error);
+  console.log('Error!', error);
 });
+
+// fetch(url, {
+//     mode: 'no-cors' // 'cors' by default
+//   })
+// .then(function(resp) {
+//     return resp.json();
+// })
+// .then(function(data) {
+//     console.log(data);
+//     let price = data.results;
+//     retur
+// })
+// .catch(function(error) {
+//     console.log('Error!', error);
+// });
