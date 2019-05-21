cae36df3d0cfdee088422d5b2d77444e3082267f
Inna Mon <inna.mon@gmail.com>
2019-05-20 19:31:07 -0500

    removed JSOn.parse and cleaned up appendData function. Trying to turn JSON object into array

--- script.js
+++ script.js
@@ -3,23 +3,28 @@ const url2 = "https://investors-exchange-iex-trading.p.rapidapi.com/stock/{symbo
 
 fetch(url)
 .then(function(response) {
-  //console.log(response);
+  // console.log(response);
   return response.json();
 })
 .then(function(data) {
-  let obj = JSON.stringify(data["Time Series (5min)"]); //removed ["2019-05-09 16:00:00"]["2. high"]
-  appendData(obj);
-  console.log(obj);
-  console.log(Array.from(obj));
+  console.log(data)
+  // let obj = JSON.stringify(data["Time Series (5min)"] ["2019-05-20 16:00:00"]["2. high"]); 
+  let obj = JSON.stringify(data["Time Series (5min)"]);
+  console.log(Array.from(obj))
+  // let obj = JSON.stringify(data);
+  
+  // appendData(obj);
 })
 .catch(function(error) {
   console.log('Error!', error);
 });
 
 function appendData(obj) {
-  let x = JSON.parse(obj);
+  // let x = JSON.parse(obj);
+  var date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
   const finances = document.getElementById('finances');
-  finances.innerText = `Daily High for 2019-05-09 4:OO PM : ${x}`;
+  finances.innerText = `Daily High for ${date} : ${obj}`; //insert current date
+  // console.log(obj);
 }
 
 // function appendData(obj) {
